468fdf2ef2a544bc5db5cf1f5b24dd68283d8df4
Dustin Toff <me@dustintoff.com>
2024-08-01 08:33:10 -0400

    Cleanup build stuff

--- build.rs
+++ build.rs
@@ -1,15 +1,4 @@
-use std::process::Command;
-
+// This is needed for the build to provide the the OUT_DIR build env flag
 fn main() {
-    // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo::rerun-if-changed=package.json");
-    println!("cargo::rerun-if-changed=package-lock.json");
-    println!("cargo::rerun-if-changed=src/web_actor/static");
-
-    assert!(Command::new("npm").args(["ci"]).status().unwrap().success());
-    assert!(Command::new("npm")
-        .args(["run", "build"])
-        .status()
-        .unwrap()
-        .success());
+    println!("cargo::rerun-if-changed=build.rs");
 }
